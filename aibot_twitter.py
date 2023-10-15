c816bd85e024a2bcaf0a4b3a4cb7c49425fc3cd9
genusglobalinc <34900625+genusglobalinc@users.noreply.github.com>
2023-10-15 16:07:07 -0400

    Add files via upload
    this is a version that works with data miner and google spreadsheets

--- aibot_twitter.py
+++ aibot_twitter.py
@@ -1,46 +1,57 @@
-import tweepy
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import openai
-import time
+#
+#Program Name: AiBot_Twitter
+#Author: Jyasi' Davis
+#Purpose: To send DMs to 400 Twitter accounts in one day
+
+# 1. Setup Environment Variables
+# 2. Download queried usernames from DataMiner
+# 3. Get bio from usernames, and validate matches
+# 4. Generate personalized Chat GPT response using DM format 
+# 5. Send DMs to 40 accounts, marking them as messaged, and add date stamp
+# 6. Do this for 10 twitter accounts
+# 7. BONUS: At 8pm, send me a message asking if the program should run again the next day.
+#           If I don't respond, run program again.
+
+#---- 1. Setup Environment Variables -------------------------------------------------------------------------
+# Required Libraries
 import requests
+import time
+import tweepy
+import openai
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
 
-#----------------------------------------------------------------------------------------------------------------
-# Function Definitions:
+# Set up Google Sheets API credentials
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('ai-bot-twitter-08dd107ad8e6.json', scope)
+client = gspread.authorize(creds)
 
-# Function to send a standard Direct Message using Twitter API v2
-def send_standard_dm(api, recipient_id, message):
-    try:
-        url = 'https://api.twitter.com/2/direct_messages/events'
-        data = {
-            "event": {
-                "type": "message_create",
-                "message_create": {
-                    "target": {
-                        "recipient_id": recipient_id
-                    },
-                    "message_data": {
-                        "text": message
-                    }
-                }
-            }
-        }
-        
-        headers = {
-            "Authorization": f"Bearer {BEARER_TOKEN}"
-        }
+#---- 2. Download queried usernames from DataMiner---------------------------------------------------------------
+# Access the spreadsheet by its title or URL
+spreadsheet = client.open('Twitter Accounts')
 
-        response = requests.post(url, json=data, headers=headers)
+# Select a specific worksheet
+worksheet = spreadsheet.worksheet('Sheet1')
 
-        if response.status_code == 201:
-            print(f"Direct message sent to recipient {recipient_id}")
-        else:
-            print(f"Failed to send DM to recipient {recipient_id}. Status code: {response.status_code}")
+# Get Twitter usernames from the spreadsheet
+scrapedUsers = worksheet.get_all_records()
 
-    except Exception as e:
-        print(f"Failed to send DM to recipient {recipient_id}: {e}")
+#---- 3. Get bio from usernames, and validate matches-------------------------------------------------------------
+def get_bio_and_validate_matches(usernames, keyword):
+    matched_usernames = []
+    
+    for username in usernames:
+        try:
+            user = api.get_user(screen_name=username)
+            bio = user.description  # Get user's bio from Twitter
+            if keyword in bio:
+                matched_usernames.append(username)
+        except tweepy.TweepError as e:
+            print(f"Error fetching user data for {username}: {str(e)}")
+    
+    return matched_usernames
 
-# Function to generate DM content using GPT-3
+#----  4. Generate personalized Chat GPT response using DM format----------------------------------------
 def generate_meeting_request_dm(account_username):
     # Define a structured message template
     template = {
@@ -68,76 +79,81 @@ def generate_meeting_request_dm(account_username):
 
     return generated_content
 
-# Function to search for users based on keywords in their profiles
-# Replace the 'search_users' function with this updated function
-def search_users(api, query):
-    search_params = {
-        'query': query,
-        'max_results': 10,  # The number of results to retrieve
-    }
-    
-    url = 'https://api.twitter.com/2/tweets/search/all?query=' + query
-    response = api.request('GET', url)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+#----  5. Send DMs to 40 accounts, marking them as messaged, and add date stamp----------------------------------------
+def send_dm_to_accounts(usernames, api):
+   
+   # Initialize total DM count
+    total_dm_count_cell = worksheet.find("Total DM Count")
+    total_dm_count = int(worksheet.cell(total_dm_count_cell.row, total_dm_count_cell.col + 1).value)
+	currentDM_count = 0
+
+    for username in usernames:
+        if currentDM_count >= 40:  # Check if the daily send limit is reached
+            break
+
+        dm_text = generate_meeting_request_dm(username)  # Generate DM text
+        try:
+            api.send_direct_message(username, text=dm_text)  # Send DM
+            print(f"Sent DM to {username}: {dm_text}")
+
+            # Update total DM count
+            currentDM_count += 1
+
+            # Mark the account as messaged by adding 'X' next to the username in the spreadsheet
+            cell_list = worksheet.findall(username)  # Find cells containing the username
+            if cell_list:
+                for cell in cell_list:
+                    # Get the row where the username is found and add 'X' in the adjacent cell
+                    row = worksheet.row_values(cell.row)
+                    row.append('X')
+                    row.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                    worksheet.insert_row(row, index=cell.row)
+
+        except tweepy.TweepError as e:
+            print(f"Error sending DM to {username}: {str(e}")
+
+    # Update the total DM count in the spreadsheet
+	total_dm_count += currentDM_count
+    worksheet.update(total_dm_count_cell.row, total_dm_count_cell.col + 1, total_dm_count)
+
+#----  6. Do this for 10 Twitter accounts----------------------------------------
+# Function to read Twitter account credentials from another sheet
+def get_twitter_credentials(sheet, credentials_sheet_name):
+    credentials_sheet = sheet.worksheet(credentials_sheet_name)
+    data = credentials_sheet.get_all_records()
+    credentials = []
+
+    for row in data:
+        credentials.append({
+            "consumer_key": row["Consumer Key"],
+            "consumer_secret": row["Consumer Secret"],
+            "access_token": row["Access Token"],
+            "access_token_secret": row["Access Token Secret"]
+        })
+
+    return credentials
+
+# Name of the sheet where you store Twitter account credentials
+credentials_sheet_name = 'Twitter Credentials'
+
+# Get the list of Twitter credentials from the other sheet
+twitter_accounts = get_twitter_credentials(spreadsheet, credentials_sheet_name)
 
 
-def search_hashtag_filter_bio_and_send_dms(api, hashtag, daily_dm_limit=40):
-    print (hashtag)
-    users = search_users(api, hashtag)
-    
-    if users:
-        for user in users['data']:
-            user_bio = user['description'].lower()
-            if any(keyword in user_bio for keyword in bio_keywords):
-                account_username = user['username']
-                recipient_id = user['id']
-                dm_content = generate_meeting_request_dm(account_username)
-                send_standard_dm(api, recipient_id, dm_content)
-                daily_dm_limit -= 1
+# Main program
+if __name__ == '__main__':
+    keyword = 'your_keyword'  # Replace with your target keyword
+    for _ in range(10):  # Repeat the process for 10 times
+        # Get Twitter account credentials from your sheet (replace 'Twitter Credentials' with the correct sheet name)
+        twitter_accounts = get_twitter_credentials(spreadsheet, 'Twitter Credentials')
 
-                if daily_dm_limit == 0:
-                    break
+        for account_credentials in twitter_accounts:
+            # Set up code's client auth using Tweepy for the current account
+            auth = tweepy.OAuthHandler(account_credentials["consumer_key"], account_credentials["consumer_secret"])
+            auth.set_access_token(account_credentials["access_token"], account_credentials["access_token_secret"])
+            api = tweepy.API(auth)
 
+            matched_usernames = get_bio_and_validate_matches(scrapedUsers, keyword)  # Step 3
+            send_dm_to_accounts(matched_usernames, api)  # Pass the API object for the current account
 
-#----------------------------------------------------------------------------------------------------------------
-# Main Code:
-
-# Initialize OpenAI with your API key
-api_key = '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'
-openai.api_key = api_key
-
-# Twitter API constant credentials
-consumer_key = '7G6aj7rnvqA26D6dwy2kvK2ui'
-consumer_secret = 'rX5mcADDJn8XswQ8bBiE6CdgUfFIh94Y8qcRyoLgHPyiRTL2MY'
-BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAOUPqgEAAAAAyZHqSFaw92wUz0KrwDzUcQfeRhQ%3DWEocFGkh76X0KrSmixBTOEZ8HvhCPfQJSDjmybtD9mzjoSrfL7'
-
-# Set up Google Sheets API credentials
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('ai-bot-twitter-08dd107ad8e6.json', scope)
-client = gspread.authorize(creds)
-
-# Access the spreadsheet by its title or URL
-spreadsheet = client.open('Twitter Accounts')
-
-# Select a specific worksheet
-worksheet = spreadsheet.worksheet('Sheet1')
-
-# Read Twitter API credentials from the spreadsheet
-data = worksheet.get_all_records()
-
-# Loop through the Twitter accounts and their access tokens
-for row in data:
-    access_token = row["Access Token"]
-    access_token_secret = row["Access Token Secret"]
-
-    # Initialize Twitter API for the current account
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-
-    # Execute the DM-sending logic for each account
-    search_hashtag_filter_bio_and_send_dms(api, "indiedev")
+            time.sleep(60)  # Sleep for 60 seconds (adjust as needed)
