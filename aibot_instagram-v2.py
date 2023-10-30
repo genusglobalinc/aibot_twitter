bc1fa5c6917eaea78961a6677c5d0609162bd3dc
genusglobalinc <34900625+genusglobalinc@users.noreply.github.com>
2023-10-30 11:59:07 -0400

    Update aibot_instagram-v2.py
    Half update

--- aibot_instagram-v2.py
+++ aibot_instagram-v2.py
@@ -1,5 +1,8 @@
 import requests
 import random
+import gspread
+import time
+import openai
 
 # Your Instagram Graph API access token
 access_token = 'your-access-token'
@@ -13,43 +16,87 @@ max_posts = 1000
 # Initialize a set to store retrieved usernames
 retrieved_usernames = set()
 
-# Run through different hashtags
-for _ in range(10):  # You can adjust the number of runs
-    # Randomly select a hashtag from the list
-    hashtag = random.choice(hashtags)
-    
-    # Set up pagination
-    next_url = f'https://graph.instagram.com/v13.0/tags/{hashtag}/recent_media?access_token={access_token}&count=10'
-    
-    while next_url and len(retrieved_usernames) < max_posts:
-        try:
-            response = requests.get(next_url)
-            if response.status_code == 200:
-                data = response.json()
-                if 'data' in data:
-                    for post in data['data']:
-                        if 'username' in post['caption']:
-                            retrieved_usernames.add(post['caption']['username'])
-                if 'paging' in data and 'next' in data['paging']:
-                    next_url = data['paging']['next']
+# Set up Google Sheets API credentials
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('your-credentials.json', scope)
+client = gspread.authorize(creds)
+
+# Open the Google Sheets document containing the list of usernames
+spreadsheet = client.open('Your Instagram Usernames Document')
+worksheet = spreadsheet.get_worksheet(0)
+
+# Instagram Graph API access token
+access_token = 'your-instagram-access-token'
+
+# Initialize the OpenAI API key
+openai.api_key = 'your-openai-api-key'
+
+# Function to validate and store usernames
+def validate_and_store_usernames():
+    # Run through different hashtags
+    for _ in range(10):  # You can adjust the number of runs
+        # Randomly select a hashtag from the list
+        hashtag = random.choice(hashtags)
+        
+        # Set up pagination
+        next_url = f'https://graph.instagram.com/v13.0/tags/{hashtag}/recent_media?access_token={access_token}&count=10'
+        
+        while next_url and len(retrieved_usernames) < max_posts:
+            try:
+                response = requests.get(next_url)
+                if response.status_code == 200:
+                    data = response.json()
+                    if 'data' in data:
+                        for post in data['data']:
+                            if 'username' in post['caption']:
+                                retrieved_usernames.add(post['caption']['username'])
+                    if 'paging' in data and 'next' in data['paging']:
+                        next_url = data['paging']['next']
+                    else:
+                        next_url = None
                 else:
-                    next_url = None
-            else:
-                print("Failed to fetch post data.")
+                    print("Failed to fetch post data.")
+                    break
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
                 break
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-            break
 
-# Convert the set back to a list for further processing
-unique_usernames = list(retrieved_usernames)
+# Function to generate and send a DM message using ChatGPT
+def generate_and_send_dm(username, access_token):
+    # Your code to generate a custom message using ChatGPT
+    generated_message = generate_message(username)
 
-# Ensure you have a list of around 400 unique usernames to message
-if len(unique_usernames) >= 400:
-    usernames_to_message = unique_usernames[:400]
-else:
-    print("Not enough unique usernames collected.")
+    # Construct the DM data
+    dm_data = {
+        'recipient_user_id': username,
+        'message': generated_message
+    }
 
-# Print the usernames to message
-for username in usernames_to_message:
-    print(username)
+    # Send the DM using the Instagram Graph API
+    response = requests.post(f'https://graph.instagram.com/v13.0/me/media/abc123/messages?access_token={access_token}', json=dm_data)
+
+    if response.status_code == 200:
+        print(f'Sent DM to {username}: {generated_message}')
+        return True
+    else:
+        print(f'Failed to send DM to {username}: {response.text}')
+        return False
+
+# Function to mark a username as "messaged" in the Google Sheets document
+def mark_as_messaged(username):
+    cell = worksheet.find(username)
+    worksheet.update_cell(cell.row, cell.col + 1, 'Messaged')
+
+# Function to iterate through and process usernames
+def process_usernames():
+    usernames = worksheet.col_values(1)  # Assuming usernames are in the first column
+    for username in usernames:
+        if username != '' and username != 'Messaged':
+            if generate_and_send_dm(username, access_token):
+                mark_as_messaged(username)
+            time.sleep(60)  # Sleep to respect Instagram's rate limits
+
+# Main program
+if __name__ == '__main':
+    validate_and_store_usernames()
+    process_usernames()
