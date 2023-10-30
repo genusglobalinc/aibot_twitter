8fcc2fa030c0608751614a3d3bf826408a96e698
genusglobalinc <34900625+genusglobalinc@users.noreply.github.com>
2023-10-30 13:06:20 -0400

    Update aibot_instagram-v2.py
    Edit

--- aibot_instagram-v2.py
+++ aibot_instagram-v2.py
@@ -60,32 +60,36 @@ def validate_and_store_usernames():
                 break
 
 # Function to generate and send a DM message using ChatGPT
-def generate_meeting_request_dm(account_username):
-	# Define a structured message template
-	template = {
-		'intro': f"Hi {account_username}, I'm looking to connect with other indie game devs on Twitter and thought we could chat!",
-		'social_proof': "I know this is random, but I actually specialize in boosting revenue using tailored funnels for game devs and streamers.",
-		'mechanism': "One thing that makes us so different is we're so sure of our process we give you free ad spend.",
-		'cta': "And more revenue means more dev time! Here's a quick run down on how we do it: [https://rb.gy/vaypj]",
-	}
-	
-	# Replace placeholders in the template with the account's username
-	for key, value in template.items():
-		template[key] = value.format(account_username=account_username)
-	
-	# Combine the template steps into the full message
-	full_message = "\n".join(template.values())
-	full_message
-	
-	# Generate additional content using GPT-3
-	response = openai.Completion.create(
-		engine="davinci",
-		prompt=full_message,
-		max_tokens=100
-	)
-	generated_content = response.choices[0].text.strip()
-	
-	return generated_content
+def generate_and_send_dm(account_username, access_token):
+    # Define a structured message template
+    template = {
+        'intro': f"Hi {account_username}, I'm looking to connect with other indie game devs on Instagram and thought we could chat!",
+        'social_proof': "I know this is random, but I actually specialize in boosting revenue using tailored funnels for game devs and streamers.",
+        'mechanism': "One thing that makes us so different is we're so sure of our process we give you free ad spend.",
+        'cta': "And more revenue means more dev time! Here's a quick run down on how we do it: [https://rb.gy/vaypj]",
+    }
+
+    # Replace placeholders in the template with the account's username
+    for key, value in template.items():
+        template[key] = value.format(account_username=account_username)
+
+    # Combine the template steps into the full message
+    full_message = "\n".join(template.values())
+    full_message
+
+    # Generate additional content using GPT-3
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=full_message,
+        max_tokens=100
+    )
+    generated_content = response.choices[0].text.strip()
+
+    # Implement code to send the DM to the user using the Instagram API
+    # Replace this with actual code to send DMs
+
+    # Return True if the DM was successfully sent, or False if not
+    return True
 
 # Function to mark a username as "messaged" in the Google Sheets document
 def mark_as_messaged(username):
