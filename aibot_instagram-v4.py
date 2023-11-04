80f8487cb9caf0aa8e2a9925403efd542892ad9d
genusglobalinc <34900625+genusglobalinc@users.noreply.github.com>
2023-11-04 12:16:35 -0400

    Update aibot_instagram-v4.py

--- aibot_instagram-v4.py
+++ aibot_instagram-v4.py
@@ -1,108 +1,79 @@
+# Import necessary libraries
 import requests
 import random
 import gspread
-import time
 import openai
-import stylegan2
-from PIL import Image, ImageDraw, ImageFont
-import os
-import cv2
-import numpy as np
 from google.oauth2.service_account import ServiceAccountCredentials
-from flask import Flask, request, jsonify
+from flask import Flask
 
-# Define your Instagram accounts and proxy configurations
+# Define bot account info for all 10 bot accounts
 accounts = [
     {
         "username": "account1",
-        "access_token": "access_token1",
+        "password": "password1",
         "proxy": {
             "ip": "proxy_ip1",
             "port": "proxy_port1",
             "username": "proxy_username1",
             "password": "proxy_password1"
-        }
-    },
-    {
-        "username": "account2",
-        "access_token": "access_token2",
-        "proxy": {
-            "ip": "proxy_ip2",
-            "port": "proxy_port2",
-            "username": "proxy_username2",
-            "password": "proxy_password2"
         },
-        # Add more accounts and proxy configurations here
-    }
+    },
+    # Add details for the other 9 accounts
 ]
 
-# Your Instagram Graph API access token
-# Add more access tokens for your accounts if needed
-access_tokens = [account["access_token"] for account in accounts]
+# Define IG Graph API access tokens
+access_tokens = ["access_token1", "access_token2", "access_token3"]  # Add more if needed
 
-# List of different hashtags
+# Define hashtags to search for
 hashtags = ['hashtag1', 'hashtag2', 'hashtag3']
 
-# Maximum number of posts to retrieve (adjust as needed)
-max_posts = 1000
+# Define limit for prospecting usernames
+prospecting_limit = 400
 
-# Initialize a set to store retrieved usernames
-retrieved_usernames = set()
+# Initialize a set to temporarily store prospected usernames
+prospected_usernames = set()
 
 # Set up Google Sheets API credentials
-# Replace with your actual Google Sheets credentials
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('your-credentials.json', scope)
 client = gspread.authorize(creds)
 
-# Open the Google Sheets document containing the list of usernames
-spreadsheet = client.open('Your Instagram Usernames Document')
+# Open the Google Sheets document housing prospects
+spreadsheet = client.open('Your Google Sheets Document')
 worksheet = spreadsheet.get_worksheet(0)
 
 # Initialize the OpenAI API key
 openai.api_key = 'your-openai-api-key'
 
-# Set up Flask for Dialogflow webhook
+# Set up Flask app for DialogFlow fulfillment
 app = Flask(__name__)
 
-# Function to validate and store usernames based on bio
-def validate_and_store_usernames():
-    for _ in range(10):  # You can adjust the number of runs
+# Flask route for DialogFlow webhook
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # Implement intent handling here
+    # Generate response based on detected intent
+    return jsonify({'fulfillmentText': 'Response from DialogFlow'})
+    
+# Define a function to find and store 400 unique usernames to Google Sheets document
+def find_and_store_usernames():
+    for _ in range(prospecting_limit):
         hashtag = random.choice(hashtags)
         next_url = f'https://graph.instagram.com/v13.0/tags/{hashtag}/recent_media?access_token={random.choice(access_tokens)}&count=10'
 
-        while next_url and len(retrieved_usernames) < max_posts:
+        while next_url and len(prospected_usernames) < prospecting_limit:
             try:
                 account = random.choice(accounts)
-                access_token = account["access_token"]
+                username = account["username"]
+                password = account["password"]
                 proxy = account["proxy"]
-                # Create a session with the proxy
-                session = requests.Session()
-                session.proxies = {
-                    'http': f'http://{proxy["username"]}:{proxy["password"]}@{proxy["ip"]}:{proxy["port"]}',
-                    'https': f'http://{proxy["username"]}:{proxy["password"]}@{proxy["ip"]}:{proxy["port"]}'
-                }
 
-                response = session.get(next_url)
-                if response.status_code == 200:
-                    data = response.json()
-                    if 'data' in data:
-                        for post in data['data']:
-                            if 'username' in post.get('caption', {}):
-                                username = post['caption']['username']
-                                bio = get_user_bio(username)
-                                keywords = ["indie game dev", "game dev"]
-                                bio_lower = bio.lower()
-                                if any(keyword in bio_lower for keyword in keywords):
-                                    retrieved_usernames.add(username)
-                    next_url = data['paging'].get('next')
-                else:
-                    print("Failed to fetch post data.")
-                    break
+                # Implement code to find usernames and store them in Google Sheets
+
+                next_url = data['paging'].get('next')
             except Exception as e:
                 print(f"An error occurred: {str(e)}")
-                break
-
+                
 # Function to send a DM using a specific account and proxy
 def send_dm(username, access_token, proxy_info):
     proxy = proxy_info["proxy"]
@@ -148,86 +119,99 @@ def send_dm(username, access_token, proxy_info):
 
     if response.status_code == 200:
         print(f'Sent DM to {username}: {generated_message}')
+        cell = worksheet.find(username)
+        worksheet.update_cell(cell.row, cell.col + 1, 'Messaged')
         return True
     else:
         print(f'Failed to send DM to {username}: {response.text}')
         return False
-
-# Function to mark a username as "messaged" in the Google Sheets document
-def mark_as_messaged(username):
-    cell = worksheet.find(username)
-    worksheet.update_cell(cell.row, cell.col + 1, 'Messaged')
-
-# Process usernames and send messages
+        
+# Function to process usernames and send messages
 def process_usernames():
     usernames = worksheet.col_values(1)  # Assuming usernames are in the first column
     for username in usernames:
         if username != '' and username != 'Messaged':
             for account in accounts:
-                if account["username"] == params["account"]:
-                    account_info = account
+                if account["username"] == username:
+                    send_dm(username, account["access_token"], account["proxy"])
+                    time.sleep(60)  # Sleep to respect Instagram's rate limits
                     break
-            else:
-                continue  # Account not found, skip
+                    
+# Function to create and post a reel
+def create_and_post_reel(bot, username, password, proxy_info):
+    # Log in to an Instagram account
+    bot.login(username=username, password=password, proxy=proxy_info)
 
-            send_dm(username, account_info['access_token'], account_info['proxy'])
-            mark_as_messaged(username)
-            time.sleep(60)  # Sleep to respect Instagram's rate limits
+    # Generate an image using StyleGAN2
+    latent_vector = stylegan2.run.generate_latent()
+    image = Gs.run(latent_vector)
 
-# Function to add text overlay
-def add_text_overlay(image, text):
-    draw = ImageDraw.Draw(image)
-    position = (50, 50)  # Adjust the position as needed
-    font = ImageFont.load_default()
-    text_color = (255, 255, 255)  # White color
-    draw.text(position, text, fill=text_color, font=font)
-    return image
+    # Generate a caption using ChatGPT
+    generated_text = generate_text_with_gpt("Your prompt here")
 
-# Function to fetch prompts from Google Sheets
-def get_prompts_from_google_sheets():
-    worksheet = gc.open('Your Spreadsheet Name').sheet1
-    prompts = worksheet.col_values(1)  # Assuming prompts are in the first column
-    return prompts
+    # Add the generated text as an overlay to the image
+    image_with_overlay = add_text_overlay(image, generated_text)
 
-# Function to generate text with ChatGPT
-def generate_text_with_gpt(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=100
-    )
-    return response.choices[0].text
-
-# Load the pre-trained StyleGAN2 model
-network_pkl = 'your_pretrained_model.pkl'
-_G, _D, Gs = stylegan2.run.load_networks(network_pkl)
-
-# Create a VideoWriter for the output video
-frame_size = (width, height)  # Replace with the actual frame size
-fps = 30  # Frames per second
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Video codec (change as needed)
-video_writer = cv2.VideoWriter('output_video.mp4', fourcc, 30.0, frame_size)  # Adjust the settings as needed
-
-if __name__ == "__main__":
-    validate_and_store_usernames()
-
-    prompts = get_prompts_from_google_sheets()
-
-    for prompt in prompts:
-        generated_text = generate_text_with_gpt(prompt)
-
-        # Generate an image using StyleGAN2
-        latent_vector = stylegan2.run.generate_latent()
-        image = Gs.run(latent_vector)
-
-        # Add the generated text as an overlay
-        image_with_overlay = add_text_overlay(image, generated_text)
-
-        # Convert the image to a frame and add it to the video
-        frame = cv2.cvtColor(np.array(image_with_overlay), cv2.COLOR_RGB2BGR)
-        video_writer.write(frame)
+    # Convert the image to a frame and add it to the video
+    frame = cv2.cvtColor(np.array(image_with_overlay), cv2.COLOR_RGB2BGR)
+    video_writer.write(frame)
 
     # Release the VideoWriter
     video_writer.release()
 
-    process_usernames()
+    video_path = 'output_video.mp4'  # Provide the path to your generated video
+
+    # Upload the video to Instagram
+    bot.upload_reel(video_path, caption=generated_text)
+
+    # Log out from the account
+    bot.logout()
+    
+# Function to generate a personalized message using ChatGPT
+def generate_personalized_message(previous_message):
+    global conversation_context
+    conversation_context.append(previous_message)
+
+    # Use ChatGPT to create a response based on the conversation context
+    response = openai.Completion.create(
+        engine="davinci",
+        messages=conversation_context,
+        max_tokens=50
+    )
+
+    # Extract the response message from ChatGPT
+    response_message = response.choices[0].message['content'].strip()
+    
+    return response_message
+
+# Variable to store the conversation context
+conversation_context = []
+
+# Function to handle the Dialogflow webhook request
+@app.route('/dialogflow-webhook', methods=['POST'])
+def dialogflow_webhook():
+    req = request.get_json()
+    
+    # Extract intent and parameters from the Dialogflow request
+    intent = req['queryResult']['intent']['displayName']
+
+    if intent == 'BookMeeting':
+        # Use ChatGPT to generate a personalized message asking for meeting details
+        personalized_message = generate_personalized_message(req['queryResult']['queryText'])
+
+        return jsonify({
+            'fulfillmentText': personalized_message
+        })
+    elif intent == 'CollectMeetingDetails':
+        # Extract parameters provided by the user
+        date = req['queryResult']['parameters']['date']
+        time = req['queryResult']['parameters']['time']
+        location = req['queryResult']['parameters']['location']
+        # You can now use the collected parameters to book the meeting and provide a response.
+        return jsonify({
+            'fulfillmentText': f'Great! We have scheduled a meeting on {date} at {time} at {location}.'
+        })
+    else:
+        # Handle other intents here if needed
+        return jsonify({'fulfillmentText': 'I am not sure how to respond to that.'})
+
