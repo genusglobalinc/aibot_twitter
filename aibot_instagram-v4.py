97a1635bf7dbb9aca0a37008118c63d4f11c4dc3
genusglobalinc <34900625+genusglobalinc@users.noreply.github.com>
2023-11-18 07:51:28 -0500

    Update aibot_instagram-v4.py

--- aibot_instagram-v4.py
+++ aibot_instagram-v4.py
@@ -11,6 +11,7 @@
 #-------------------------------------------------------------------------------------------------------------------
 
 import requests
+import sys
 import random
 import gspread
 import signal
