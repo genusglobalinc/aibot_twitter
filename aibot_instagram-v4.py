eec2005b8fa04d0e94f0704c468325170a1918ae
genusglobalinc <34900625+genusglobalinc@users.noreply.github.com>
2023-11-18 21:53:06 -0500

    Update aibot_instagram-v4.py

--- aibot_instagram-v4.py
+++ aibot_instagram-v4.py
@@ -175,6 +175,7 @@ def find_and_store_usernames(account):
                 else:
                     update_global_status("Error: " + response.status_code)
                     print("Failed to fetch post data. Status Code: {response.status_code}")
+                    print()
                     break
             except Exception as e:
                 print(f"An error occurred: {str(e)}")
