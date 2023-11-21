329df3e0ef4493914717aefa438869aa1b81155f
genusglobalinc <34900625+genusglobalinc@users.noreply.github.com>
2023-11-21 08:47:16 -0500

    Update aibot_instagram-v4.py
    Debug updates

--- aibot_instagram-v4.py
+++ aibot_instagram-v4.py
@@ -179,10 +179,13 @@ def find_and_store_usernames(account):
                     print(f"Failed to fetch post data. Status Code: {response.status_code}")
                     print(f"Message: {response.text}")
                     print()
+                    prospecting_failed = True
                     update_global_status(f"Debug message: Prospecting process has ended. {len(prospected_usernames)} prospects found.")
                     print()
                     break
             except Exception as e:
+                prospecting_failed = True
+                update_global_status(f"Debug message: Prospecting process has ended. {len(prospected_usernames)} prospects found.")
                 print(f"An error occurred: {str(e)}")
                 break
     
