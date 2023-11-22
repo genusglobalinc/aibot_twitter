9b28fd61d98e5bef7cfc488b37a3ec44223dc235
genusglobalinc <34900625+genusglobalinc@users.noreply.github.com>
2023-11-22 12:02:14 -0500

    Update igbot_v1.py

--- igbot_v1.py
+++ igbot_v1.py
@@ -69,12 +69,7 @@ def schedule_posts(posts_type, schedule_date):
 
 # Main script to execute the Instagram Graph API workflow
 def instagram_graph_api_script():
-    # 1-5. Get project, posts, comment, bots, and hashtags sheets data from Google Drive
-    project_sheet_data = get_google_sheets_data("project_sheet")
-    posts_sheet_data = get_google_sheets_data("posts_sheet")
-    comment_sheet_data = get_google_sheets_data("comment_sheet")
-    bots_sheet_data = get_google_sheets_data("bots_sheet")
-    hashtags_sheet_data = get_google_sheets_data("hashtags_sheet")
+    # ... (Existing code)
 
     # Loop for 30 times (Step 7)
     for _ in range(30):
@@ -86,35 +81,7 @@ def instagram_graph_api_script():
             posts_data = search_posts_by_hashtag(hashtag)
             # Store relevant data in posts sheet
 
-    # 8. Process comments and store prospects
-    for post_data in posts_sheet_data:
-        process_comments(post_data['media_id'], "keyword")
-
-    # 9. Generate comments and mark as contacted
-    for username in comment_sheet_data:
-        generate_comments_and_mark_contacted(username)
-
-    # 10. 4x a day, get messages from bot account and respond to inquiries
-    for _ in range(4):
-        check_and_respond_to_dm_inquiries(bots_sheet_data['bot_account'])
-
-    # 11. For each uncontacted username, get two random posts, generate comment on one, and mark follow-up date
-    follow_up_with_usernames(uncontacted_usernames, contacted_usernames)
-
-    # 12. Check if any contacted usernames have a follow-up
-    # (Implementation depends on your specific logic for follow-ups)
-
-    # 13. Post batch of ad posts with TensorFlow model
-    post_ad_posts_with_tensorflow()
-
-    # 14. Generate and post story
-    generate_and_post_story()
-
-    # 15. Check to see if new comments, posts, or stories need to be scheduled
-    schedule_posts("comments", datetime.now() + timedelta(days=1))
-    schedule_posts("posts", datetime.now() + timedelta(days=2))
-    schedule_posts("stories", datetime.now() + timedelta(days=random.randint(1, 3)))
-
+    # ... (Continue with the existing logic)
 
 # Flask app for DialogFlow fulfillment
 app = Flask(__name__)
@@ -126,14 +93,23 @@ Session(app)
 # Rest of your existing code, including routes, functions, and server setup
 # ...
 
-# Example: Function to handle the Dialogflow webhook request
+# Example# Example: Function to handle the Dialogflow webhook request
 @app.route('/dialogflow-webhook', methods=['POST'])
 def dialogflow_webhook():
     req = request.get_json()
-    
-    # Your existing code for handling Dialogflow intents
-    # ...
 
+    # Placeholder code, replace with actual DialogFlow intent handling
+    intent = req['queryResult']['intent']['displayName']
+    if intent == 'YourIntentName':
+        # Implement logic for handling the specific intent
+        fulfillment_text = 'Your fulfillment text here.'
+    else:
+        # Handle other intents if needed
+        fulfillment_text = 'Default fulfillment text.'
+
+    return jsonify({'fulfillmentText': fulfillment_text})
+
+# ... (Existing code)
 # Function to gracefully shutdown Flask server for code updates
 def signal_handler(sig, frame):
     print('Shutting down gracefully...')
